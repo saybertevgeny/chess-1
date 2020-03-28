0748d0c304e0e04638bbb10cfee2e5bff65ad354
Evgeny Saybert <sajberte@mail.ru>
2020-03-28 13:46:26 +0300

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,17 +14,7 @@ public class Main {
         ChessBoard b = new ChessBoard();
         b.print();
 
-        for (int i = 0; i<b.getBoard()[6][1].possibleMovesList(6,1).size(); i++){
-            b.getBoard()[6][1].possibleMovesList(6,1).get(i).print();
-        }
-
-        /*Checker c = new Checker();
-        ArrayList<Move> m = new ArrayList<>();
-        ArrayList<Move> mm = c.allowedMoves(b.getBoard()[6][1], b, 6,1,m);
-        for (int i = 0; i< mm.size(); i++){
-            mm.get(i).print();
-            System.out.println();
-        }*/
+        
 
         Game g = new Game();
         g.startGame();
